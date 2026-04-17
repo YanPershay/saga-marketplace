b9dd2d4c9625c40f7c999ab002a96236afad7835
Yan Pershay <53472127+YanPershay@users.noreply.github.com>
2026-04-17 13:33:15 +0200

    chore: fix gitignore and remove build artifacts

--- services/catalog/Catalog.Application/obj/Debug/net9.0/Catalog.Application.AssemblyInfo.cs
+++ services/catalog/Catalog.Application/obj/Debug/net9.0/Catalog.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+eddf2a866b3cf9e423277b698e4ac5137fb215d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7538ef3d43f8cb0c6d0bc696ce976ac9c0ef8a8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Catalog.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
